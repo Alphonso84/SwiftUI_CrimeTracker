6eced2bddbbd7fc9ba6eadfe0a108c7375041fe2
Alphonso Sensley II <alphonso.sensleyjr@gmail.com>
2020-06-26 15:13:04 -0700

    Initial Commit

---
+++ SwiftUI_CrimeTracker/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  SwiftUI_CrimeTracker
+//
+//  Created by Alphonso Sensley II on 6/26/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!").padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUI_CrimeTracker/SwiftUI_CrimeTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_CrimeTrackerApp.swift
+//  SwiftUI_CrimeTracker
+//
+//  Created by Alphonso Sensley II on 6/26/20.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_CrimeTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
