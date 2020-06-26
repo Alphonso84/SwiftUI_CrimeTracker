d95a1805ed52748b313b220f708a8785507ed3a7
Alphonso Sensley II <alphonso.sensleyjr@gmail.com>
2020-06-26 16:19:37 -0700

    First Commit

--- SwiftUI_CrimeTracker/ContentView.swift
+++ SwiftUI_CrimeTracker/ContentView.swift
@@ -9,7 +9,42 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!").padding()
+        VStack {
+            
+            Image("Icon").padding(.bottom)
+           
+            Picker(selection: .constant(1), label: Text("Picker")
+                    .multilineTextAlignment(.center)
+                    .padding(0.0)) /*@START_MENU_TOKEN@*/{
+                Text("Oakland").tag(1)
+                Text("Fremont").tag(2)
+                Text("Hayward").tag(3)
+                Text("Berkeley").tag(4)
+                Text("San Leandro").tag(5)
+                Text("Livermore").tag(6)
+                Text("Pleasanton").tag(7)
+                Text("Alameda").tag(8)
+                Text("Union City").tag(9)
+                Text("Dublin").tag(10)
+            }.frame(width:100, alignment: .bottom).padding(.bottom)
+            
+           
+            Button(action: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/{}/*@END_MENU_TOKEN@*/) {
+                Text("Get Crime Data")
+                    .frame(width: 300.0, height: 50)
+            }
+           
+            .background(Color.gray)
+            .foregroundColor(Color.white)
+            .frame(width: 300.0, height: 50.0)
+            .cornerRadius(15)
+            
+            
+            Image("Image").resizable().frame(width: 300, height: 100, alignment: .center) .aspectRatio(contentMode: .fit)
+            
+        }/*@END_MENU_TOKEN@*/
+        
+        
     }
 }
 
