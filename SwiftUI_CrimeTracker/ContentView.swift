610375aed48571003150c156f75446959f3f2fbe
Alphonso Sensley II <alphonso.sensleyjr@gmail.com>
2020-06-29 11:45:33 -0700

    Adding NavigationLink for secondary screen

--- SwiftUI_CrimeTracker/ContentView.swift
+++ SwiftUI_CrimeTracker/ContentView.swift
@@ -8,49 +8,47 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var getCrimeButtonPressed = false
     var body: some View {
-        VStack {
-            
-            Image("Icon").padding(.bottom)
-           
-            Picker(selection: .constant(1), label: Text("Picker")
-                    .multilineTextAlignment(.center)
-                    .padding(0.0)) /*@START_MENU_TOKEN@*/{
-                Text("Oakland").tag(1)
-                Text("Fremont").tag(2)
-                Text("Hayward").tag(3)
-                Text("Berkeley").tag(4)
-                Text("San Leandro").tag(5)
-                Text("Livermore").tag(6)
-                Text("Pleasanton").tag(7)
-                Text("Alameda").tag(8)
-                Text("Union City").tag(9)
-                Text("Dublin").tag(10)
-            }.frame(width:100, alignment: .bottom).padding(.bottom)
-            
-           
-            Button(action: {
-               print("Button Tapped")
-            
+        NavigationView {
+            VStack {
+                Image("Icon").padding(.bottom)
                 
-                    
-            }) {
-                Text("Hi There")
-            } .frame(width: 300.0, height: 50)
-           
-            .background(Color.gray)
-            .foregroundColor(Color.white)
-            .frame(width: 300.0, height: 50.0)
-            .cornerRadius(15)
+                Picker(selection: .constant(1), label: Text("Picker")
+                        .multilineTextAlignment(.center)
+                        .padding(0.0)) {
+                    Text("Oakland").tag(1)
+                    Text("Fremont").tag(2)
+                    Text("Hayward").tag(3)
+                    Text("Berkeley").tag(4)
+                    Text("San Leandro").tag(5)
+                    Text("Livermore").tag(6)
+                    Text("Pleasanton").tag(7)
+                    Text("Alameda").tag(8)
+                    Text("Union City").tag(9)
+                    Text("Dublin").tag(10)
+                }.frame(width:100, alignment: .bottom).padding(.bottom)
+                
+                NavigationLink(destination: CrimeListView()) {
+                    Text("Get Crimes")
+                }
+                .frame(width: 300.0, height: 50)
+                .background(Color.gray)
+                .foregroundColor(Color.white)
+                .frame(width: 300.0, height: 50.0)
+                .cornerRadius(15)
+                
+                
+               
+               
             
-            
-            Image("Image").resizable().frame(width: 300, height: 100, alignment: .center) .aspectRatio(contentMode: .fit)
-            
-        }/*@END_MENU_TOKEN@*/
-        
-        
+                }
+                
+                Image("Image").resizable().frame(width: 300, height: 100, alignment: .center) .aspectRatio(contentMode: .fit)
+                
+            }
+        }
     }
-}
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
---
+++ SwiftUI_CrimeTracker/CrimeListView.swift
@@ -0,0 +1,20 @@
+//
+//  CrimeListView.swift
+//  SwiftUI_CrimeTracker
+//
+//  Created by Alphonso Sensley II on 6/28/20.
+//
+
+import SwiftUI
+
+struct CrimeListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CrimeListView_Previews: PreviewProvider {
+    static var previews: some View {
+        CrimeListView()
+    }
+}
